37d147dd70046874f2a77ce63334ed58799e01ff
abonilla1 <amariabrown015@gmail.com>
2020-12-14 12:24:07 -0600

    obtaining data from api

--- controllers/heroes.js
+++ controllers/heroes.js
@@ -3,8 +3,11 @@ const axios = require("axios");
 
 
 module.exports = {
+    index,
     new: newHero,
     search,
+    show,
+    
 }
 
 
@@ -16,5 +19,16 @@ function newHero(req, res) {
 }
 
 function search(req, res) {
-    
+    axios.get(`https://superheroapi.com/api/106220101357993/search/${req.body.query}`)
+    .then((response) => {
+        console.log(response.data.results)
+        res.render("heroes/new", {
+            user: req.user,
+            results: response.data.results
+        })
+    })
 }
+
+function show(req, res) {
+    axios.get(`https://super/api/106220101357993/`)
+})
--- routes/heroes.js
+++ routes/heroes.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 const heroesCtrl = require("../controllers/heroes")
 
 router.get("/new", isLoggedIn, heroesCtrl.new)
+router.get('/', isLoggedIn, heroesCtrl.index)
+router.get("/:id", isLoggedIn, heroesCtrl.show)
+router.post("/search", isLoggedIn, heroesCtrl.search)
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
