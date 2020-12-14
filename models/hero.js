c015beaa7e91832dfd8a531d0f4487a2f77bab25
abonilla1 <amariabrown015@gmail.com>
2020-12-14 10:26:30 -0600

    basic models complete

---
+++ models/hero.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+
+const ratingSchema = new Schema (
+    { 
+        ratedBy: String,
+        rating: {type: Number, min: 1, max: 10 },
+        content: String,
+    }, 
+    {
+        timestamps: true
+    }    
+);
+
+const heroSchema = new Schema (
+    {
+        name: String,
+        apiId: Number,
+        imageUrl: String,
+        stats: [],
+        starredBy:[{type: Schema.Types.ObjectId, ref: "User"}],
+        rating: [ratingSchema]
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Hero", heroSchema)
--- models/user.js
+++ models/user.js
@@ -4,9 +4,11 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     name: String,
+    nickname: String,
     email: String,
     avatar: String,
     googleId: String,
+    superTeam: [{type: Schema.Types.ObjectId, ref: "Hero"}],
   },
   {
     timestamps: true,
